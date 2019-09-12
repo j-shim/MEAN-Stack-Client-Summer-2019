abda46205719ac0e170d6d531aad0facce3b9106
June Shim <jys2@sfu.ca>
2019-09-12 20:22:12 +0900

    Setup navbar, main, register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
