628a025534526ac1f2b57455564f0af87f7bcc66
June Shim <jys2@sfu.ca>
2019-09-15 21:35:04 +0900

    Setup login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { RestClientService } from '../rest-client/rest-client.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  private _loginForm: FormGroup;
+  constructor(
+    private _restClientService: RestClientService,
+    private _formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this._loginForm = this._formBuilder.group({
+      email: '',
+      password: ''
+    });
   }
 
+  public onSubmit(loginForm: FormGroup): void {
+    const baseUrl = 'http://localhost:3000';
+    const apiEndpoint = '/api/users/login';
+    this._restClientService.post(baseUrl + apiEndpoint, loginForm)
+      .subscribe(resp => {
+        console.log('########');
+        console.log(resp);
+      }, err => {
+        console.error('Error 123:', err);
+      });
+
+    this._loginForm.reset();
+  }
 }
