2b5ecefaaf9966258d14fdbba3c41cff3e28870d
June Shim <jys2@sfu.ca>
2019-11-11 11:38:13 -0800

    Cleanup RegisterService

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { RegisterService } from './register.service';
+import { RestClientService } from '../rest-client/rest-client.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -9,9 +10,11 @@ import { RegisterService } from './register.service';
 })
 export class RegisterComponent implements OnInit {
   private _registerForm: FormGroup;
+
   constructor(
-    private _registerService: RegisterService,
-    private _formBuilder: FormBuilder
+    private _restClientService: RestClientService,
+    private _formBuilder: FormBuilder,
+    private _router: Router
   ) { }
 
   ngOnInit() {
@@ -25,7 +28,21 @@ export class RegisterComponent implements OnInit {
   }
 
   public onSubmit(): void {
-    this._registerService.httpPostRegister(this._registerForm.value);
+    // stop here if form is invalid
+    if (this._registerForm.invalid) {
+      console.error('RegisterForm invalid');
+      return;
+    }
+
+    const baseUrl = 'http://localhost:3000';
+    const apiEndpoint = '/api/users/register';
+    this._restClientService.post(baseUrl + apiEndpoint, this._registerForm.value)
+      .subscribe(resp => {
+        console.log('Registration Successful:', resp);
+        this._router.navigate(['/login']);
+      }, err => {
+        console.error('Error on Register:', err);
+      });
 
     this._registerForm.reset();
   }
