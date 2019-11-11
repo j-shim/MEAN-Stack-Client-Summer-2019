4ce9ef7dd2a49c91fa1857cc2a0bfd78dd1bd0c4
June Shim <jys2@sfu.ca>
2019-11-11 11:41:37 -0800

    Cleanup / commenting out

--- src/app/rest-client/rest-client.service.ts
+++ src/app/rest-client/rest-client.service.ts
@@ -18,7 +18,7 @@ export class RestClientService {
 
   public post(url: string, body: any): Observable<any> {
     return this._http.post(url, body).pipe(
-      retry(3), // retry a failed request up to 3 times
+      // retry(3), // retry a failed request up to 3 times
       catchError(this._handleError)
     );
   }
@@ -31,19 +31,22 @@ export class RestClientService {
     return this._http.delete(url);
   }
 
-  private _handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`, error.error
-      );
-    }
+  private _handleError(error: HttpErrorResponse): Observable<never> {
+    // For dev purposes only
+    console.error(error);
+
+    // if (error.error instanceof ErrorEvent) {
+    //   // A client-side or network error occurred. Handle it accordingly.
+    //   console.error('Error:', error.error.message);
+    // } else {
+    //   // The backend returned an unsuccessful response code.
+    //   // The response body may contain clues as to what went wrong,
+    //   console.error(
+    //     `Backend returned code ${error.status}, ` +
+    //     `Body was: ${error.error}`, error.error
+    //   );
+    // }
     // return an observable with a user-facing error message
-    return throwError('Something bad happened; please try again later.');
+    return throwError(error.error.message);
   }
 }
