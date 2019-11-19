beb01cdd42a54a0f1fb602fc034de6d12a2ae9f8
June Shim <jys2@sfu.ca>
2019-11-19 00:24:08 -0800

    Fix BASE_URL_PROD typo (critical)

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -2,7 +2,7 @@ import { environment } from 'src/environments/environment';
 
 export class SETTINGS {
   private static BASE_URL_DEV = 'http://localhost:3000';
-  private static BASE_URL_PROD = 'https://hi-janice.herokuapp.com/'; // to be changed later
+  private static BASE_URL_PROD = 'https://hi-janice.herokuapp.com'; // to be changed later
 
   public static BASE_URL = environment.production ? SETTINGS.BASE_URL_PROD : SETTINGS.BASE_URL_DEV;
 
