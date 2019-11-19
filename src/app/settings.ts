0a193d5511a7727dd8c94e3d93d0d94212f090b5
June Shim <jys2@sfu.ca>
2019-11-19 00:06:46 -0800

    Add base url for production

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,6 +1,12 @@
-export const SETTINGS = {
-  BASE_URL: 'http://localhost:3000',
-  API_USERS_REGISTER: '/api/users/register',
-  API_USERS_LOGIN: '/api/users/authenticate',
-  API_POSTS: '/api/posts/'
-};
+import { environment } from 'src/environments/environment';
+
+export class SETTINGS {
+  private static BASE_URL_DEV = 'http://localhost:3000';
+  private static BASE_URL_PROD = 'https://hi-janice.herokuapp.com/'; // to be changed later
+
+  public static BASE_URL = environment.production ? SETTINGS.BASE_URL_PROD : SETTINGS.BASE_URL_DEV;
+
+  public static API_USERS_REGISTER = '/api/users/register';
+  public static API_USERS_LOGIN = '/api/users/authenticate';
+  public static API_POSTS = '/api/posts/';
+}
