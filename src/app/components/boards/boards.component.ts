a3ef767d92c9a1d0e34d228efdc5d984fb4ae5a5
June Shim <jys2@sfu.ca>
2019-11-19 16:49:59 -0800

    Add pagination for BoardsComponent. Also display posts with table format

--- src/app/components/boards/boards.component.ts
+++ src/app/components/boards/boards.component.ts
@@ -12,6 +12,10 @@ import { Post } from 'src/app/_models/post';
 export class BoardsComponent implements OnInit {
 
   public posts: Post[];
+  private allPosts: Post[];
+  // tslint:disable-next-line:no-inferrable-types
+  private _postsPerPage: number = 5; // should be const
+  private _currentPage: number;
 
   constructor(
     private _restClientService: RestClientService,
@@ -19,18 +23,25 @@ export class BoardsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this._currentPage = 1;
     this.getAllPosts();
   }
 
   public getAllPosts(): void {
     this._restClientService.get<Post[]>(SETTINGS.BASE_URL + SETTINGS.API_POSTS)
       .subscribe(posts => {
-        this.posts = posts;
-        console.log('getAllPosts Successful:', this.posts);
+        this.allPosts = posts;
+        this.page(this._currentPage);
+        console.log('getAllPosts Successful:', this.allPosts);
         this._alertService.alert(true, 'getAllPosts Successful');
       }, err => {
         console.error('Error on getAllPosts:', err);
         this._alertService.alert(false, `Error on getAllPosts: ${typeof err === 'string' ? err : JSON.stringify(err)}`);
       });
+  }
+
+  public page(page: number): void {
+    this._currentPage = page;
+    this.posts = this.allPosts.slice((page - 1) * this._postsPerPage, page * this._postsPerPage);
   }
 }
