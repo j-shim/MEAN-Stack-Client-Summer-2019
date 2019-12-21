cdd2e379e9e85c9e5ac6b18176ae74417c8d881c
June Shim <jys2@sfu.ca>
2019-12-20 17:55:52 -0800

    Finish flight paths for Montreal and Halifax

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user/user.service';
 
 // [Province, Capital, stroke-dashoffset] (Refer to .scss comments)
-const prov = [['bc', 'victoria', '160'],
-              ['ab', 'edmonton', '130'],
-              ['sk', 'regina', '110'],
-              ['mb', 'winnipeg', '90'],
-              ['on', 'ottawa', '160']];
+const prov = [['bc', 'victoria', '196'],
+              ['ab', 'edmonton', '169'],
+              ['sk', 'regina', '137'],
+              ['mb', 'winnipeg', '111'],
+              ['on', 'ottawa', '160'],
+              ['qc', 'montreal', '164'],
+              ['ns', 'halifax', '196']];
 
 @Component({
   selector: 'app-main',
@@ -93,6 +95,36 @@ export class MainComponent implements OnInit {
           for (let j = 0; j < pathElem.length; j++) {
             pathElem[j]['style']['transition'] = 'none';
             pathElem[j]['style']['stroke-dashoffset'] = prov[4][2];
+          }
+        });
+      } else if (provinceElem[i].classList.value.includes(prov[5][0])) {
+        provinceElem[i].addEventListener('mouseover', () => {
+          const pathElem = document.getElementsByClassName(prov[5][1]);
+          for (let j = 0; j < pathElem.length; j++) {
+            pathElem[j]['style']['transition'] = 'stroke-dashoffset 2.5s ease';
+            pathElem[j]['style']['stroke-dashoffset'] = '0';
+          }
+        });
+        provinceElem[i].addEventListener('mouseout', () => {
+          const pathElem = document.getElementsByClassName(prov[5][1]);
+          for (let j = 0; j < pathElem.length; j++) {
+            pathElem[j]['style']['transition'] = 'none';
+            pathElem[j]['style']['stroke-dashoffset'] = prov[5][2];
+          }
+        });
+      } else if (provinceElem[i].classList.value.includes(prov[6][0])) {
+        provinceElem[i].addEventListener('mouseover', () => {
+          const pathElem = document.getElementsByClassName(prov[6][1]);
+          for (let j = 0; j < pathElem.length; j++) {
+            pathElem[j]['style']['transition'] = 'stroke-dashoffset 2.5s ease';
+            pathElem[j]['style']['stroke-dashoffset'] = '0';
+          }
+        });
+        provinceElem[i].addEventListener('mouseout', () => {
+          const pathElem = document.getElementsByClassName(prov[6][1]);
+          for (let j = 0; j < pathElem.length; j++) {
+            pathElem[j]['style']['transition'] = 'none';
+            pathElem[j]['style']['stroke-dashoffset'] = prov[6][2];
           }
         });
       }
