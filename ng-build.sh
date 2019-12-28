967325a3aecab01ea2ba3e2d01f2e0fe8f862c29
June Shim <jys2@sfu.ca>
2019-12-27 16:51:04 -0800

    Update 'npm run build' script (Server folder should have nothing to commit)

--- ng-build.sh
+++ ng-build.sh
@@ -4,6 +4,7 @@ if [ -d ../MEAN-Stack-Server-Summer-2019/public ]; then
   rm -r ../MEAN-Stack-Server-Summer-2019/public
 fi
 
-ng build --prod --output-path=dist/public && mv dist/public ../MEAN-Stack-Server-Summer-2019
+# Make sure there is nothing to commit on the server folder before running the line below
+ng build --prod --output-path=dist/public && mv dist/public ../MEAN-Stack-Server-Summer-2019 && cd ../MEAN-Stack-Server-Summer-2019 && git add --all && git commit -m "Update public/" && git push && git push heroku master
 
 exit 0
