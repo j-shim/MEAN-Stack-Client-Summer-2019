19f90738fb614c8cb0334df0a2c838950e6454f7
June Shim <jys2@sfu.ca>
2019-12-29 20:20:36 -0800

    On inquiry submit, InquiryService checks and blocks sending email if needed

--- src/app/components/inquiry/inquiry.component.ts
+++ src/app/components/inquiry/inquiry.component.ts
@@ -4,6 +4,7 @@ import { RestClientService } from 'src/app/services/rest-client/rest-client.serv
 import { Router } from '@angular/router';
 import { AlertService } from 'src/app/services/alert/alert.service';
 import { SETTINGS } from 'src/app/settings';
+import { InquiryService } from 'src/app/services/inquiry/inquiry.service';
 
 @Component({
   selector: 'app-inquiry',
@@ -16,6 +17,7 @@ export class InquiryComponent implements OnInit {
   constructor(
     private _restClientService: RestClientService,
     private _alertService: AlertService,
+    private _inquiryService: InquiryService,
     private _formBuilder: FormBuilder,
     private _router: Router
     ) { }
@@ -37,7 +39,11 @@ export class InquiryComponent implements OnInit {
   public onSubmit(): void {
     // stop here if form is invalid
     if (this.inquiryForm.invalid) {
-      this._alertService.alert(false, 'Invalid Form');
+      this._alertService.alert(false, '문의 내용이 올바르지 않습니다.');
+      return;
+    }
+    if (this._inquiryService.isTooEarlyToSubmitAgain(Date.now())) {
+      this._alertService.alert(false, '하루에 한번만 신청 가능합니다.');
       return;
     }
 
@@ -45,12 +51,12 @@ export class InquiryComponent implements OnInit {
 
     this._restClientService.post(SETTINGS.BASE_URL + SETTINGS.API_MAILER, { htmlString })
       .subscribe(resp => {
-        console.log('Inquiry Successful:', resp);
-        this._alertService.alert(true, 'Inquiry Successful');
+        this._alertService.alert(true, '신청이 성공적으로 완료 되었습니다.');
         this._router.navigate(['/']);
       }, err => {
-        console.error('Error on Register:', err);
-        this._alertService.alert(false, `Error on Register: ${typeof err === 'string' ? err : JSON.stringify(err)}`);
+        console.error('Error on Inquiry:', err);
+        this._alertService.alert(false, `문제가 발생하였습니다. 나중에 다시 시도하시기 바랍니다.`);
+        // this._alertService.alert(false, `문제가 발생하였습니다. 나중에 다시 시도하시기 바랍니다.: ${typeof err === 'string' ? err : JSON.stringify(err)}`);
       });
 
     this.inquiryForm.reset();
