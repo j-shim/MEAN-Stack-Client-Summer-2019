440846854b1d402d0fabc3dcd929af2a3c9f76de
June Shim <jys2@sfu.ca>
2020-01-01 16:02:57 -0800

    TEST PURPOSE: Add test function for debugging touch event on iOS (TODO: Revert this commit later)

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -17,4 +17,13 @@ export class NavbarComponent implements OnInit {
     const elem: HTMLElement = document.getElementById(id);
     elem.classList.toggle('toggled');
   }
+  public test(eventBinding: string, event: any): void {
+    console.log(`Event binding: ${eventBinding}`);
+    console.log(event);
+    console.log(event.type);
+    alert(`Testing:
+            Event binding: ${eventBinding},
+            Event fired: ${event},
+            Event type: ${event.type}`);
+  }
 }
