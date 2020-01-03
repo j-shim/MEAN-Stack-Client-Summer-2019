50598196a6f03a4a98d4038d4b86bdcc469a2f1c
June Shim <jys2@sfu.ca>
2020-01-02 17:45:39 -0800

    Hamburger menu reverts back to original state on dropdown link click

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -17,22 +17,7 @@ export class NavbarComponent implements OnInit {
     const elem: HTMLElement = document.getElementById(id);
     elem.classList.toggle('toggled');
   }
-  public test(eventBinding: string, event: any, id: string): void {
-    console.log(`Event binding: ${eventBinding}`);
-    console.log(event);
-    console.log(event.type);
-    // alert(`Testing:
-    //         Event binding: ${eventBinding},
-    //         Event fired: ${event},
-    //         Event type: ${event.type}`);
-    if (eventBinding === 'click1') {
-      document.getElementById(id)['style']['display'] = 'none !important';
-    }
-    else if (eventBinding === 'click2') {
-      setTimeout(() => {
-        document.getElementById(id)['style']['display'] = 'none !important';
-      }, 1000);
-    }
+  public onLinkClick(): void {
     const toggler = document.querySelector('.navbar-toggler');
     toggler['checked'] = false;
   }
