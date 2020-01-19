3129331100da605023f89eb8be494022cc7c3b81
June Shim <jys2@sfu.ca>
2020-01-18 16:01:37 -0800

    Remove routing for login, register, boards and posts component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { BoardsComponent } from './components/boards/boards.component';
-import { PostsComponent } from './components/posts/posts.component';
 import { StaticComponent } from './components/static/static.component';
 import { InquiryComponent } from './components/inquiry/inquiry.component';
 
 
 const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'boards/:bid', component: BoardsComponent },
-  { path: 'boards/:bid/:pid', component: PostsComponent },
   { path: 'page/:id', component: StaticComponent },
   { path: 'inquiry', component: InquiryComponent },
 
