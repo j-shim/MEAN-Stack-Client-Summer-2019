13f531318ca00ca822fe52a5832c54a41045097b
June Shim <jys2@sfu.ca>
2020-01-23 13:53:21 -0800

    Update build script for deploying to AWS EC2 instead of Heroku

--- ng-build.sh
+++ ng-build.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-if [ -d ../MEAN-Stack-Server-Summer-2019/public ]; then
-  rm -r ../MEAN-Stack-Server-Summer-2019/public
+# if [ -d ../MEAN-Stack-Server-Summer-2019/public ]; then
+#   rm -r ../MEAN-Stack-Server-Summer-2019/public
+# fi
+
+# # Make sure there is nothing to commit on the server folder before running the line below
+# ng build --prod --output-path=dist/public && mv dist/public ../MEAN-Stack-Server-Summer-2019 && cd ../MEAN-Stack-Server-Summer-2019 && git add --all && git commit -m "Update public/" && git push && git push heroku master
+
+
+
+# The script above is used for deploying to Heroku
+
+# The script below is used for deploying to AWS EC2
+
+if [ -d dist/mean-client ]; then
+  echo "Removing ./dist/mean-client ..."
+  rm -r dist/mean-client
 fi
 
-# Make sure there is nothing to commit on the server folder before running the line below
-ng build --prod --output-path=dist/public && mv dist/public ../MEAN-Stack-Server-Summer-2019 && cd ../MEAN-Stack-Server-Summer-2019 && git add --all && git commit -m "Update public/" && git push && git push heroku master
-
-exit 0
+ng build --prod
+echo "Next step: Run awsscp ..."
