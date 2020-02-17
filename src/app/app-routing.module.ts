1b90c6d9f0645588e424cb7d98af85ee4dd4e69e
June Shim <jys2@sfu.ca>
2020-02-16 22:08:36 -0800

    Move ReviewsComponent to the bottom of MainComponent, and adjust styling/layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { StaticComponent } from './components/static/static.component';
 import { InquiryComponent } from './components/inquiry/inquiry.component';
-import { ReviewsComponent } from './components/reviews/reviews.component';
 
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'page/:id', component: StaticComponent },
   { path: 'inquiry', component: InquiryComponent },
-  { path: 'reviews', component: ReviewsComponent },
 
   // otherwise redirect to home
   { path: '**', redirectTo: '', pathMatch: 'full' }
